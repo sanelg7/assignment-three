a477876b09ebd0df2d71d452f65b35d27f5b2a2e
Ege Şanel <sanelege@gmail.com>
2023-02-03 00:21:16 +0300

    first commit

---
+++ src/test/java/com/definexpracticum/assignmentthree/AssignmentThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definexpracticum.assignmentthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
