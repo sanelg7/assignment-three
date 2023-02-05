4fddbae7a141ca8939a91d635d636f91fc7a9f7a
Ege Şanel <sanelege@gmail.com>
2023-02-05 19:23:56 +0300

    Completed functionality.

--- src/main/java/com/definexpracticum/assignmentthree/model/HourlyWeather.java
+++ src/main/java/com/definexpracticum/assignmentthree/model/HourlyWeather.java
@@ -4,15 +4,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record HourlyWeather(
         String resolvedAddress,
         @JsonProperty("tzoffset") int timeZone,
 
-        List<Day> days
+        List<Day> days,
+
+        List<Hour> hours
 
 
 ) {
+    public HourlyWeather{
+        hours = days.stream()
+                .flatMap(day -> day.hours().stream())
+                .collect(Collectors.toList());
+    }
 
 }
